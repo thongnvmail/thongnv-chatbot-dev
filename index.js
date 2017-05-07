8b9acd95e056b35a831e9950cd98d123b600100a
Nguyen Van Thong <thongnvmail@gmail.com>
2017-05-07 18:58:30 +0700

    UPdate index.js code tu FB messaging

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ Ban Can Co:
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+
+//Bien lay gia tri token tren FB thongnvbot
+const token = process.env.FB_VERIFY_TOKEN
+const acsess = process.env.FB_ACCESS_TOKEN
 //
 app.set('port',(process.env.PORT || 5000))
 //
@@ -25,11 +29,47 @@ app.get('/', function (req, res){
 
 //
 app.get('/webhook/', function(req, res){
-	if(req.query['hub.verify_token'] ==='thongnvbot'){
+	if(req.query['hub.verify_token'] ==='token'){
 		res.send(req.query['hub.challenge'])
 		}
 	res.send('khong co gi');
 	})
+//	
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+  
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log("Message data: ", event.message);
+}
+	
 //
 app.listen(app.get('port'), function(){
 	console.log('Running on port', app.get('port'))

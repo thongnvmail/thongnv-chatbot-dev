367d164cf1ec346f9ae9086fce5eab05794cbdd8
Nguyen Van Thong <thongnvmail@gmail.com>
2017-05-08 00:07:40 +0700

    aaaaaaaaaaaaaa

--- server.js
+++ server.js
@@ -123,9 +123,9 @@ function sendTextMessage(recipientId, messageText) {
 }
 
 function callSendAPI(messageData) {
-  	request({
+    request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: access },
+    qs: { access_token: process.env.FB_ACCESS_TOKEN },
     method: 'POST',
     json: messageData
 

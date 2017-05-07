5f5156b780250470ed6c8e2632f80fee91a32372
Nguyen Van Thong <thongnvmail@gmail.com>
2017-05-07 18:18:29 +0700

    Doi token thanh >> thongnvbot

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ app.get('/', function (req, res){
 
 //
 app.get('/webhook/', function(req, res){
-	if(req.query['hub.verify_token'] ===
-	'my_voice_is_my_password_verify_me'){
+	if(req.query['hub.verify_token'] ==='thongnvbot'){
 		res.send(req.query['hub.challenge'])
 		}
 	res.send('khong co gi');

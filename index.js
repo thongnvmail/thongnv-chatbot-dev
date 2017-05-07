f5f6374d8aff204cb64602cd53acf2bdc9bd9be2
Nguyen Van Thong <thongnvmail@gmail.com>
2017-05-07 17:09:59 +0700

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+/*
+Ban Can Co:
+
+  1. Heroku account
+  2. FB account
+  3. FB page
+  4. install/update node
+  5. Heroku commad line 
+
+*/
+
+const express = require('express')
+const bodyPaser = require('body-parser')
+const app = express()
+//
+app.set('port',(process.env.PORT || 5000))
+//
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+//
+app.get('/', function (req, res){
+	res.send('Thongnv hello')
+	})
+
+
+//
+app.get('/webhook/', function(req, res){
+	if(req.query['hub.verify_token'] ===
+	'my_voice_is_my_password_verify_me'){
+		res.send(req.query['hub.challenge'])
+		}
+	res.send('khong co gi');
+	})
+//
+app.listen(app.get('port'), function(){
+	console.log('Running on port', app.get('port'))
+	})

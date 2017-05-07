f1c4e3d7639fa48bb117fe122f9027fe2a6b8fae
Nguyen Van Thong <thongnvmail@gmail.com>
2017-05-07 20:32:32 +0700

    Them ham nhan va tra loi

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 
 //Bien lay gia tri token tren FB thongnvbot
 const token = process.env.FB_VERIFY_TOKEN
-const acsess = process.env.FB_ACCESS_TOKEN
+const accsess = process.env.FB_ACCESS_TOKEN
 //
 app.set('port',(process.env.PORT || 5000))
 //
@@ -29,7 +29,7 @@ app.get('/', function (req, res){
 
 //
 app.get('/webhook/', function(req, res){
-	if(req.query['hub.verify_token'] ==='token'){
+	if(req.query['hub.verify_token'] ===token){
 		res.send(req.query['hub.challenge'])
 		}
 	res.send('khong co gi');
@@ -64,10 +64,42 @@ app.post('/webhook', function (req, res) {
     res.sendStatus(200);
   }
 });
-  
+  // them ham tra loi tu FB
 function receivedMessage(event) {
   // Putting a stub for now, we'll expand it in the following steps
-  console.log("Message data: ", event.message);
+
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+ 
+}
+ 
+ 
 }
 	
 //

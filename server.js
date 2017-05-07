ccb117f20f7e97e7c68721896677ba12e0ae3a9f
Nguyen Van Thong <thongnvmail@gmail.com>
2017-05-07 23:38:02 +0700

    aaaaaaaaaaaa

--- server.js
+++ server.js
@@ -29,12 +29,16 @@ app.get('/', function (req, res){
 
 
 //
-app.get('/webhook/', function(req, res){
-	if(req.query['hub.verify_token'] === token){
-		res.send(req.query['hub.challenge'])
-		}
-	res.send('khong co gi')
-	})
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === token) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
 	
 //	code tren fb 5. Receive Messages
 app.post('/webhook', function (req, res) {

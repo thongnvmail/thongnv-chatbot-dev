534fd5c339e060e7579eb72c2f30214c39ee120a
Nguyen Van Thong <thongnvmail@gmail.com>
2017-05-08 16:45:17 +0700

    Chay bot co ban

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const request = require('request')
 const app = express()
 
 //Bien lay gia tri token tren FB thongnvbot
-const token = process.env.FB_VERIFY_TOKEN
+const token = process.env.FB_VERIFY_TOKEN	
 const access = process.env.FB_ACCESS_TOKEN
 //
 app.set('port',(process.env.PORT || 5000))
@@ -73,10 +73,75 @@ app.post('/webhook', function (req, res) {
 });
   
 function receivedMessage(event) {
-  // Putting a stub for now, we'll expand it in the following steps
-  console.log("Message data: ", event.message);
-}
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
 
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+//
+function sendGenericMessage(recipientId, messageText) {
+  // To be expanded in later sections
+}	
+//
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+//
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: access },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
 //
 app.listen(app.get('port'), function(){
 	console.log('Running on port', app.get('port'))

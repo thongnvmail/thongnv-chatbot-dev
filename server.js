0cf1694eba83bf4f0f285b6da81d3789b2763eb0
Nguyen Van Thong <thongnvmail@gmail.com>
2017-05-08 17:22:56 +0700

    them ham moi

--- server.js
+++ server.js
@@ -101,10 +101,30 @@ function receivedMessage(event) {
     }
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
+  }else if (event.postback) {
+          receivedPostback(event); 
   }
 }
-// mau tin co cau truc
-function sendGenericMessage(recipientId) {
+// chua biet sau
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback 
+  // button for Structured Messages. 
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " + 
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to 
+  // let them know it was successful
+  sendTextMessage(senderID, "Postback called");
+}
+// Them text co cau truc hinh anh 
+function sendGenericMessage(recipientId, messageText) {
+  function sendGenericMessage(recipientId) {
   var messageData = {
     recipient: {
       id: recipientId
@@ -150,7 +170,7 @@ function sendGenericMessage(recipientId) {
 
   callSendAPI(messageData);
 }
-	
+}	
 //
 function sendTextMessage(recipientId, messageText) {
   var messageData = {

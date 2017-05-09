d0d4c24eddd82c9e96b9c617d7daea640628b79a
Nguyen Van Thong <thongnvmail@gmail.com>
2017-05-09 11:53:57 +0700

    check error

--- server.js
+++ server.js
@@ -101,27 +101,10 @@ function receivedMessage(event) {
     }
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
-  }else if (event.postback) {
-          receivedPostback(event); 
   }
 }
-// chua biet sau
-function receivedPostback(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfPostback = event.timestamp;
 
-  // The 'payload' param is a developer-defined field which is set in a postback 
-  // button for Structured Messages. 
-  var payload = event.postback.payload;
 
-  console.log("Received postback for user %d and page %d with payload '%s' " + 
-    "at %d", senderID, recipientID, payload, timeOfPostback);
-
-  // When a postback is called, we'll send a message back to the sender to 
-  // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
-}
 // Them text co cau truc hinh anh 
 function sendGenericMessage(recipientId, messageText) {
   function sendGenericMessage(recipientId) {
